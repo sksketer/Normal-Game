a38f60e08e1f949438d53f0619cb82cc17cc2529
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-01-11 00:11:50 +0530

    Create Print.java

---
+++ CardGame in java/Game1 (using ArrayList)/src/operations/Print.java
@@ -0,0 +1,21 @@
+package operations;
+
+public class Print
+{
+    public void Sop(String msg)
+    {
+        System.out.println(msg);
+    }
+    public void Sop(int msg)
+    {
+        System.out.println(msg);
+    }
+    public void pf(String msg)
+    {
+        System.out.print(msg);
+    }
+    public void pf(int msg)
+    {
+        System.out.print(msg);
+    }
+}
